10f7f0cc9dfd0798bf19f94cc9c9f5b2b09bf1a0
frcroth <frcroth@gmail.com>
2022-03-19 16:23:08 +0100

    Move ui to seperate script

---
+++ src/scripts/ui.ts
@@ -0,0 +1,10 @@
+alert("Loaded successfully")
+/*
+let canvas = document.getElementById('map-canvas') as
+  HTMLCanvasElement;
+let context = canvas.getContext("2d");
+if (context) {
+  //context.rect(0, 0, 200, 200);
+  // context.fill()
+  context.drawImage(document.getElementById("map-image") as HTMLImageElement, 0, 0);
+}*/
