2e23d389e1b740d4b28e364d775d087b84c62622
forUnity <39216451+forUnity@users.noreply.github.com>
2022-03-20 11:01:49 +0100

    Refactor DataCenterView to class
     to class for import Architecture

--- src/scripts/ui/DatacenterViewNew.ts
+++ src/scripts/ui/DatacenterViewNew.ts
@@ -1,90 +1,105 @@
 import * as Plotly from 'plotly.js';
-import { Datacenter, DeadlineTask } from '../simulation';
-var plotEl = document.getElementById('plotly')! as any;
-// layout.shapes.push();
-var layout = {
-  margin: { t: 0 },
-  xaxis: { fixedrange: true },
-  yaxis: { fixedrange: true },
-  showlegend: false,
-  // hovermode: 'closest'
-  //shapes??
-  shapes: [] as any
-};
-
-var shapeTaskIds: number[] = []; //shapeIndex -> taskId
+import { Datacenter, DeadlineTask, Task } from '../simulation';
 
 
-var config = { responsive: true, staticPlot: false, displayModeBar: false };
-
-var plot = Plotly.newPlot(plotEl, [{
-  x: [1, 2, 3, 4, 5],
-  y: [1, 2, 4, 8, 16]
-}], layout, config);
-
-
-
-function addRect(x: number, y: number, w: number, h: number, color: string, isInProgress: boolean, taskId: number): void {
-  let dashType = isInProgress ? 'dot' : 'solid';
-  layout.shapes.push(
-    {
-      type: 'rect',
-      // x-reference is assigned to the x-values
-      // xref: 'x',
-      // // y-reference is assigned to the plot paper [0,1]
-      // yref: 'paper',
-      x0: x,
-      y0: y,
-      x1: x + w,
-      y1: y + h,
-      fillcolor: color,
-      opacity: 0.2,
-      dash: dashType
-      //   line: {
-      //     color: 'rgb(55, 128, 191)',
-      //     width: 3
-      //     //dash: 'dot'
-      //   }
+class DataCenterView {
+  plotEl: any;
+  layout: any;
+  shapeToTask: Task[] = [];
+  constructor() {
+    this.plotEl = document.getElementById('plotly')! as any;
+    // layout.shapes.push();
+    this.layout = {
+      margin: { t: 0 },
+      xaxis: { fixedrange: true },
+      yaxis: { fixedrange: true },
+      showlegend: false,
+      // hovermode: 'closest'
+      //shapes??
+      shapes: [] as any
+    };
+    //move up maybe?
+    this.plotEl.on('plotly_click', function (data: any) {
+      // do something;
+      // (this as DataCenterView).clickedAtTime(data.points[0].x);
+      console.log("clicked at time ", data.points[0].x);
 
     });
-  shapeTaskIds.push(taskId);
-}
 
-
-//move up maybe?
-plotEl.on('plotly_click', function (data: any) {
-  // do something;
-  clickedAtTime(data.points[0].x);
-});
-function clickedAtTime(timeSelected: number) {
-  console.log("clicked at time ", timeSelected);
-  //call other stuff
-}
-
-//mouse click down in general
-plotEl.onclick = function (e: any) {
-  if (e) {
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left; //x position within the element.
-    var y = e.clientY - rect.top;  //y position within the element.
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
-    //find in which task the click happened
+    //mouse click down in general
+    this.plotEl.onclick = function (e: any) {
+      if (e) {
+        var rect = e.target.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
+        console.log("Left? : " + x + " ; Top? : " + y + ".");
+        //find in which task the click happened
+        for (let i = 0; i < this.layout.shapes.length; i++) {
+          let shape = this.layout.shapes[i];
+          if (shape.x0 <= x && shape.x1 >= x && shape.y0 <= y && shape.y1 >= y) {
+            console.log("Clicked on task ", shape.task.name);
+          }
+        }
+      }
+    };
   }
-};
+  reset(dc: Datacenter, time: number) {
+    this.layout.shapes = [];
+    this.shapeToTask = []; //shapeIndex -> taskId
+
+    this.plotTasks(dc, time);
 
 
-function plotDatacenter(dc: Datacenter, time: number) {
-  layout.shapes = [];
-  shapeTaskIds = [];
-  for (let i = 0; i < dc.tasks.length; i++) {
-    if (dc.tasks[i] instanceof DeadlineTask) {
-      var task = dc.tasks[i] as DeadlineTask;
-      addRect(task.startTime, 0, task.duration, task.workLoad, '#ff0000', task.active, task.id);
-    } else {
-      addRect(0, 0, 24, dc.tasks[i].workLoad, '#ff0000', dc.tasks[i].active, dc.tasks[i].id);
+    var config = { responsive: true, staticPlot: false, displayModeBar: false };
+
+    var plot = Plotly.newPlot(this.plotEl, [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16]
+    }], this.layout, config);
+  }
+
+  addRect(x: number, y: number, w: number, h: number, color: string, isInProgress: boolean, task: Task): void {
+    let dashType = isInProgress ? 'dot' : 'solid';
+    this.layout.shapes.push(
+      {
+        type: 'rect',
+        // x-reference is assigned to the x-values
+        // xref: 'x',
+        // // y-reference is assigned to the plot paper [0,1]
+        // yref: 'paper',
+        x0: x,
+        y0: y,
+        x1: x + w,
+        y1: y + h,
+        fillcolor: color,
+        opacity: 0.2,
+        dash: dashType
+        //   line: {
+        //     color: 'rgb(55, 128, 191)',
+        //     width: 3
+        //     //dash: 'dot'
+        //   }
+
+      });
+    this.shapeToTask.push(task);
+  }
+
+
+  plotTasks(dc: Datacenter, time: number) {
+    this.layout.shapes = [];
+    this.shapeToTask = [];
+    //plot tasks at time
+    for (let i = 0; i < dc.tasks.length; i++) {
+      if (dc.tasks[i] instanceof DeadlineTask) {
+        var task = dc.tasks[i] as DeadlineTask;
+        if (task.startTime >= time && task.duration + task.startTime < time) {
+          this.addRect(task.startTime, 0, task.duration, task.workLoad, '#ff0000', task.active, task);
+        } else {
+          this.addRect(0, 0, 24, dc.tasks[i].workLoad, '#ff0000', dc.tasks[i].active, dc.tasks[i]);
+        }
+      }
     }
   }
 }
-
-export { plotDatacenter };
+export { DataCenterView };
 
