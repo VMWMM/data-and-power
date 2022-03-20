0aef213bb7386b3044d6f764f812a8a88b965e48
forUnity <39216451+forUnity@users.noreply.github.com>
2022-03-20 11:18:15 +0100

    Minor Fixes

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,7 @@
 //test graph plot
 // const dcV = require("../scripts/ui/DatacenterView.ts");
 // Scripts
+import { DataCenterView } from "../scripts/ui/DatacenterView";
 import { SimulationManager } from "./simulation";
 import { UIManager } from "./ui/ui";
 
@@ -8,7 +9,7 @@ var simulationManager = new SimulationManager();
 simulationManager.initialize();
 
 var uiManager = new UIManager(simulationManager);
-
+var dataCenterView = new DataCenterView();
 
 //for(let i=0; i<10; i++){
 //  simulationManager.simulateTurn();
--- src/scripts/ui/DatacenterView.ts
+++ src/scripts/ui/DatacenterView.ts
@@ -1,134 +1,108 @@
-
-// import { Datacenter, ScheduledTask } from "../simulation";
-
-// let canvas = document.getElementById("datacenterViewCanvas")! as HTMLCanvasElement
-// const context = canvas.getContext("2d")!;
-
-// // context.translate(0, canvas.height);
-// // context.scale(1, -1);
-// const height = canvas.height;
-// const width = canvas.width;
+import * as Plotly from 'plotly.js';
+import { Datacenter, DeadlineTask, Task } from '../simulation';
 
 
-// interface Color {
-//   r: number;
-//   g: number;
-//   b: number;
-//   a: number;
-// }
+class DataCenterView {
+  plotEl: any;
+  layout: any;
+  shapeToTask: Task[] = [];
 
-// function getColorStringC(color: Color): string {
-//   return `rgba(${color.r},${color.g},${color.b}, ${color.a})`;
-// }
-// const plotGraphColor = { r: 0, g: 0, b: 0, a: 255 };
-// const plotUncertainColor = { r: 0, g: 110, b: 0, a: 120 };
-// const plotLineThickness = 2;
-
-// const plotBgColor = { r: 255, g: 255, b: 255, a: 255 };
-// const plotAxisColor = { r: 100, g: 100, b: 100, a: 255 };
-
-// const plotStepsInAdvance = 24;
-// const xPixelsPerStep = width / (plotStepsInAdvance - 1);
-// const yAxisScale = 1000;
-// const yAxisOffset = 1;
-// function viewDatacenter(dc: Datacenter, currentSimulationTime: number) {
-//   const plotableWorkloadPower = generatePlotValueOfFunc((t: number) => dc.getCurrentPowerReq(t), currentSimulationTime, currentSimulationTime + plotStepsInAdvance, 10);
-
-//   context.filter = "blur(0px)";
-//   drawGraphBg();
-//   drawAxisTexts("Time", "Value");
-//   // const plotValues = {{1_value,1:uncertainty}} as plotValue[];
-//   const plotValues = generatePlotValueOfFunc((x: number) => x, 0, 1, plotStepsInAdvance);
-//   // context.fillStyle = getColorStringC({r:255,g:0,b:0, a:255});
-//   // plotFunctionWithUncertainty(plotValues, 0, 1, plotGraphColor, plotUncertainColor);
+  currentStartHour = 0;
 
 
-//   plotFunction(plotableWorkloadPower, 0, 10, plotGraphColor); //func WIP params a bit weird still, need better timestep...
-// }
+  constructor() {
+    this.plotEl = document.getElementById('plotly')! as any;
+    // layout.shapes.push();
+    this.layout = {
+      margin: { t: 0 },
+      xaxis: { fixedrange: true },
+      yaxis: { fixedrange: true },
+      showlegend: false,
+      // hovermode: 'closest'
+      //shapes??
+      shapes: [] as any
+    };
+    //move up maybe?
+    this.plotEl.on('plotly_click', function (data: any) {
+      // do something;
+      // (this as DataCenterView).clickedAtTime(data.points[0].x);
+      console.log("clicked at time ", data.points[0].x + 1);
+    });
+
+    //mouse click down in general
+    this.plotEl.onclick = function (e: any) {
+      if (e) {
+        var rect = e.target.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
+        console.log("Left? : " + x + " ; Top? : " + y + ".");
+        //find in which task the click happened
+        for (let i = 0; i < this.layout.shapes.length; i++) {
+          let shape = this.layout.shapes[i];
+          if (shape.x0 <= x && shape.x1 >= x && shape.y0 <= y && shape.y1 >= y) {
+            console.log("Clicked on task ", shape.task.name);
+          }
+        }
+      }
+    };
+  }
+  reset(dc: Datacenter, time: number) {
+    this.layout.shapes = [];
+    this.shapeToTask = []; //shapeIndex -> taskId
+
+    this.plotTasks(dc, time);
 
 
+    var config = { responsive: true, staticPlot: false, displayModeBar: false };
 
-// const distOfUpAxis = width / 20;
-// const distOfLeftAxis = height / 20;
-// function drawGraphBg() {
-//   context.fillStyle = getColorStringC(plotBgColor);
-//   ownFillRect(0, 0, width, height);
-//   context.fillStyle = getColorStringC(plotGraphColor);
-//   ownFillRect(distOfUpAxis, distOfLeftAxis, 1, height);
-//   ownFillRect(distOfUpAxis, distOfLeftAxis, width, 1);
-// }
+    var plot = Plotly.newPlot(this.plotEl, [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16]
+    }], this.layout, config);
+  }
 
-// function drawAxisTexts(xAxisName: string, yAxisName: string) {
-//   context.fillStyle = getColorStringC(plotAxisColor);
-//   context.font = "20px Times New Roman";
-//   ownFillText(xAxisName, width - distOfUpAxis, distOfLeftAxis / 2);
-//   ownFillText(yAxisName, distOfUpAxis / 4, height - distOfLeftAxis);
-// }
+  addRect(x: number, y: number, w: number, h: number, color: string, isInProgress: boolean, task: Task): void {
+    let dashType = isInProgress ? 'dot' : 'solid';
+    this.layout.shapes.push(
+      {
+        type: 'rect',
+        // x-reference is assigned to the x-values
+        // xref: 'x',
+        // // y-reference is assigned to the plot paper [0,1]
+        // yref: 'paper',
+        x0: x,
+        y0: y,
+        x1: x + w,
+        y1: y + h,
+        fillcolor: color,
+        opacity: 0.2,
+        dash: dashType
+        //   line: {
+        //     color: 'rgb(55, 128, 191)',
+        //     width: 3
+        //     //dash: 'dot'
+        //   }
+
+      });
+    this.shapeToTask.push(task);
+  }
 
 
+  plotTasks(dc: Datacenter, time: number) {
+    this.layout.shapes = [];
+    this.shapeToTask = [];
+    //plot tasks at time
+    for (let i = 0; i < dc.tasks.length; i++) {
+      if (dc.tasks[i] instanceof DeadlineTask) {
+        var task = dc.tasks[i] as DeadlineTask;
+        if (task.startTime >= time && task.duration + task.startTime < time) {
+          this.addRect(task.startTime, 0, task.duration, task.workLoad, '#ff0000', task.active, task);
+        } else {
+          this.addRect(0, 0, 24, dc.tasks[i].workLoad, '#ff0000', dc.tasks[i].active, dc.tasks[i]);
+        }
+      }
+    }
+  }
+}
+export { DataCenterView };
 
-// function generatePlotValueOfFunc(func: (x: number) => number, start: number, end: number, valuesPerStep: number) {
-//   let plotValues: plotValue[] = [];
-//   let testValue = 0;
-//   for (let i = start; i < end; i += 1 / valuesPerStep) {
-//     testValue += Math.random() * 1 - 0.5;
-//     testValue = Math.max(0, testValue);
-
-//     plotValues.push({ value: func(i), uncertainty: 0 });
-//   }
-//   return plotValues;
-// }
-
-
-// type plotValue = { value: number, uncertainty: number };
-// function plotFunction(plotValues: plotValue[], startIndex: number, valuesPerStep: number, lineColor: Color) {
-//   var lastXPos = 0;
-//   var lastYPos = plotValues[startIndex].value;
-//   for (let i = startIndex; i < plotValues.length; i++) {
-//     const element = plotValues[i];
-//     const xPos = (i / valuesPerStep) * xPixelsPerStep;
-//     const yPos = ((element.value - yAxisOffset) / yAxisScale) * height;
-//     ownStroke(lastXPos, lastYPos, xPos, yPos, lineColor);
-//     lastXPos = xPos;
-//     lastYPos = yPos;
-//   }
-// }
-
-// function plotFunctionWithUncertainty(plotValues: plotValue[], startIndex: number, valuesPerStep: number, lineColor: Color, fillColor: Color) {
-//   for (let i = startIndex; i < plotValues.length; i++) {
-//     const element = plotValues[i];
-//     //log plotValue at i
-//     console.log("plotValue at " + i + ": " + element.value + " with uncertainty " + element.uncertainty);
-//     const xPos = (i / valuesPerStep) * xPixelsPerStep;
-//     const yPos = ((element.value - yAxisOffset) / yAxisScale) * height;
-//     plotYValue(xPos, yPos, (element.uncertainty) / yAxisScale, lineColor, fillColor);
-//   }
-// }
-// function plotYValue(xPos: number, yPos: number, uncertainty: number, lineColor: Color, fillColor: Color) {
-//   xPos += distOfUpAxis;
-//   yPos += distOfLeftAxis;
-//   context.fillStyle = getColorStringC(fillColor);
-//   ownFillRect(xPos, (yPos - (uncertainty / 2)), xPixelsPerStep, uncertainty);
-//   context.fillStyle = getColorStringC(lineColor);
-//   ownFillRect(xPos, yPos + Math.ceil(uncertainty / 2), xPixelsPerStep, plotLineThickness);
-//   ownFillRect(xPos, yPos - Math.ceil(uncertainty / 2), xPixelsPerStep, plotLineThickness);
-// }
-
-// function ownFillRect(x: number, y: number, xs: number, ys: number) {
-//   context.fillRect(x, height - y - ys, xs, ys);
-// }
-// function ownFillText(str: string, x: number, y: number) {
-//   context.fillText(str, x, height - y);
-// }
-
-// function ownStroke(fromX: number, fromY: number, toX: number, toY: number, color: Color) {
-//   context.strokeStyle = getColorStringC(color);
-//   context.beginPath();
-//   context.moveTo(fromX, height - fromY);
-//   context.lineTo(toX, height - toY);
-//   context.stroke();
-// }
-
-// const testDatacenter = new Datacenter(0, "Test", [0, 0], 50, 1000, 1, []);
-// testDatacenter.tasks = [new ScheduledTask(1, 100, -1, 4), new ScheduledTask(40, 0, -1, 0)];
-// viewDatacenter(testDatacenter, 4);
