0b4fe58c4d2db25dcba31bf4b4c0ebf5cc228044
n-koenig <nils.koenig01@gmail.com>
2022-03-20 11:34:47 +0100

    Add JSON import functions

--- src/scripts/simulation.ts
+++ src/scripts/simulation.ts
@@ -1,3 +1,5 @@
+import * as data from "../simulationData.json";
+
 class SimulationManager {
   datacenters!: Datacenter[];
   powersources!: Powersource[];
@@ -5,19 +7,54 @@ class SimulationManager {
   constructor() {
   }
   initialize() {
+    this.powersources = [];
+    data.powersources.forEach(ps => {
+      this.powersources.push(
+        new Powersource(
+          ps.id, 
+          ps.name, 
+          ps.position, 
+          PowersourceType[ps.type]
+        ));
+    });
+    this.datacenters = []
+    data.datacenters.forEach(dc => {
+      this.datacenters.push(
+        new Datacenter(
+          dc.id, 
+          dc.name, 
+          dc.position, 
+          dc.baseconsumption, 
+          dc.maxWorkLoad, 
+          dc.workLoadToPowerFac, 
+          dc.distToDataCenters,
+          []
+        ));
+    });
+    data.datacenters.forEach(dc => {
+      let sources = [];
+      dc.powersources.forEach(ps => {
+            sources.push(this.powersources[ps])
+          });
+      this.datacenters[dc.id].powersources = sources;
+    });
+    console.log(this.powersources)
+    /*
     this.powersources = [
-      new Powersource("German Bay Offshore Wind Park", [54.6, 7.2], PowersourceType.WIND),
-      new Powersource("Alpine Dams", [47.3, 10.1], PowersourceType.HYDRO),
-      new Powersource("Norwegian Hydropower", [61.9, 7.1], PowersourceType.HYDRO),
-      new Powersource("French Solar", [45.3, 1.6], PowersourceType.SUN),
-      new Powersource("Walney Offshore Wind Farm", [53.9, -3.57], PowersourceType.WIND),
+      new Powersource(0, "German Bay Offshore Wind Park", [54.6, 7.2], PowersourceType.WIND),
+      new Powersource(1, "Alpine Dams", [47.3, 10.1], PowersourceType.HYDRO),
+      new Powersource(2, "Norwegian Hydropower", [61.9, 7.1], PowersourceType.HYDRO),
+      new Powersource(3, "French Solar", [45.3, 1.6], PowersourceType.SUN),
+      new Powersource(4, "Walney Offshore Wind Farm", [53.9, -3.57], PowersourceType.WIND),
     ];
     this.datacenters = [
       new Datacenter(0, "Berlin", [52, 13], 10, 100, 1, [1, 5], [this.powersources[2], this.powersources[0]]),
       new Datacenter(1, "Paris", [48.8, 2.3], 30, 200, 2, [1, 3], [this.powersources[3]]),
       new Datacenter(2, "Ireland", [53.3, -6.6], 5, 50, 1.5, [5, 3], [this.powersources[4]]),
-
     ];
+    console.log(this.powersources)
+    console.log(this.datacenters)
+    */
     this.currentTimeStamp = 0;
   }
   simulateTurn() {
@@ -102,17 +139,20 @@ enum PowersourceType {
 type powerAtTimeFunction = (time: number) => number;
 
 class Powersource {
+  id: number;
   name: string;
   position: [number, number];
   powerType: PowersourceType;
   // estPowerAtTime: powerAtTimeFunction = (time: number) => 0;
   // estPowerOverTime: number[] = [];
   constructor(
+    id: number,
     name: string,
     position: [number, number],
     powerType: PowersourceType
     //estPowerOverTime: number[], 
   ) {
+    this.id = id;
     this.name = name;
     this.position = position;
     this.powerType = powerType;
